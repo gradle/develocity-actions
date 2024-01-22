8c23c7a19b210a1dd9384eb3ed74091a47a5c296
Jerome Prinet <jprinet@gradle.com>
2024-01-22 12:11:00 +0100

    Inject Maven capture extension through environment instead of lib/ext

--- build-scan-setup-maven/src/main.ts
+++ build-scan-setup-maven/src/main.ts
@@ -1,38 +1,59 @@
 import * as core from '@actions/core'
+import path from "path"
 
 import * as errorHandler from '../../build-scan-shared/src/error'
 import * as input from '../../build-scan-shared/src/setup/input'
-import * as io from '../../build-scan-shared/src/io'
-import * as layout from './layout'
 import * as maven from '../../build-scan-shared/src/buildTool/maven'
-import * as wrapper from './wrapper'
+
+const MAVEN_BUILD_SCAN_CAPTURE_EXTENSION = 'maven-build-scan-capture-extension'
+const MAVEN_BUILD_SCAN_CAPTURE_EXTENSION_JAR = `${MAVEN_BUILD_SCAN_CAPTURE_EXTENSION}.jar`
+
+const ENV_KEY_MAVEN_OPTS = 'MAVEN_OPTS'
+const MAVEN_OPTS_EXT_CLASS_PATH = '-Dmaven.ext.class.path'
 
 /**
  * Main entrypoint for the action
  */
-export async function run(): Promise<void> {
+export function run(): void {
     try {
-        // Init wrapper if needed
-        await wrapper.init()
+        // Configure environment to inject capture extension on Maven builds
+        configureEnvironment()
 
         // Propagate environment variables to subsequent steps
         input.exportVariables(maven.mavenBuildTool)
-
-        // Copy Maven capture extension
-        await copyMavenCaptureExtensionToMavenHome()
     } catch (error) {
         errorHandler.handle(error)
     }
 }
 
-async function copyMavenCaptureExtensionToMavenHome(): Promise<void> {
-    // Retrieve extension source and target
-    const captureExtensionFilePath = layout.mavenBuildScanCaptureExtensionSource()
-    const mavenHomeLibExtPath = await layout.mavenBuildScanCaptureExtensionTarget()
+function configureEnvironment(): void {
+    const captureExtensionSourcePath = path.resolve(
+        __dirname,
+        '..',
+        '..',
+        '..',
+        MAVEN_BUILD_SCAN_CAPTURE_EXTENSION,
+        'dist',
+        MAVEN_BUILD_SCAN_CAPTURE_EXTENSION_JAR
+    )
 
-    // Copy Maven extension in $MAVEN_HOME/lib/ext
-    core.debug(`Copy ${captureExtensionFilePath} to ${mavenHomeLibExtPath}`)
-    io.copyFileSync(captureExtensionFilePath, mavenHomeLibExtPath)
+    const mavenOptsCurrent = process.env[ENV_KEY_MAVEN_OPTS]
+
+    let mavenOptsNew = `${MAVEN_OPTS_EXT_CLASS_PATH}=${captureExtensionSourcePath}`
+    if(mavenOptsCurrent) {
+        const extClassPathIndex = mavenOptsCurrent.indexOf(`${MAVEN_OPTS_EXT_CLASS_PATH}=`)
+        if(extClassPathIndex !== -1) {
+            // MAVEN_OPTS already configured with -Dmaven.ext.class.path
+            mavenOptsNew = mavenOptsCurrent.substring(0, extClassPathIndex) + mavenOptsNew + path.delimiter + mavenOptsCurrent.substring(extClassPathIndex + `${MAVEN_OPTS_EXT_CLASS_PATH}=`.length)
+        } else {
+            // MAVEN_OPTS already configured without -Dmaven.ext.class.path
+            mavenOptsNew = `${mavenOptsCurrent} ${mavenOptsNew}`
+        }
+    } else {
+        // MAVEN_OPTS not configured
+    }
+
+    core.exportVariable(ENV_KEY_MAVEN_OPTS, mavenOptsNew)
 }
 
 run()
